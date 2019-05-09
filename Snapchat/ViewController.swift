e7ee44fdfbc2220a8d088e4c16fcc96f91d63187
Bladimir Huanca <bladimir.huanca@tecsup.edu.pe>
2019-05-09 15:39:48 -0500

    Laboratorio terminado con los siguientes archivos: Instalacion Firebase en IOS, vista terminada y logeo terminado

--- Snapchat/ViewController.swift
+++ Snapchat/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
-class ViewController: UIViewController {
+class inicarSesionViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) {
+            (user, error) in
+            print("Intentando Iniciar Sesion")
+            if error != nil {
+                print("Se presento el siguiente error: \(error!)")
+            }else{
+                print("Inicio de sesion exitoso")
+            }
+        }
+    }
+    
 
 }
 
